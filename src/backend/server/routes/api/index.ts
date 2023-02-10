2c8feee902eb2e6d7a6c457b445dcb9019fe0f01
rinsuki <428rinsuki+git@gmail.com>
2023-02-10 21:23:36 +0900

    feat: support CORS

--- src/backend/server/routes/api/index.ts
+++ src/backend/server/routes/api/index.ts
@@ -3,6 +3,16 @@ import { Router } from "piyo"
 import v1Router from "./v1/index.js"
 
 const router = new Router()
+router.all("(.*)", (ctx, next) => {
+    ctx.set("Access-Control-Allow-Origin", "*")
+    ctx.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+    ctx.set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    if (ctx.method === "OPTIONS") {
+        ctx.status = 200
+        return
+    }
+    return next()
+})
 router.useRouter("/v1", v1Router)
 
 export default router
